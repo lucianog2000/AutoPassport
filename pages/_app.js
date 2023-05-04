5401d9bd20cffe8cf9b1e49b14de0bca3f5e3632
Luciano <legarcia2@teco.com.ar>
2023-05-03 23:13:31 -0300

    Se instala Chakra UI y se avanza con el maquetado

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@components/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '@components/components/Navbar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
