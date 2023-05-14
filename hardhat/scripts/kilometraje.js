b24b1353b16f187bc647c8f2d72427bc7e177dd8
wolffhack <69741055+wolffhack@users.noreply.github.com>
2023-05-14 13:22:11 -0600

    contratoHibrido

---
+++ hardhat/scripts/kilometraje.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const auto = await hre.ethers.getContractFactory("AutoPassport");
+  console.log("Deployeando contracto...");
+  const carro = await auto.deploy();
+  await carro.deployed();
+  console.log("Contrato deployeado en la direccion", carro.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
