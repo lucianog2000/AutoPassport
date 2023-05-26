5c720d247878410932d2c69a7ab323f34223d4da
Luciano <legarcia2@teco.com.ar>
2023-05-25 23:01:43 -0300

    Se realiza comunicación entre el backend y el smart contract

--- backend-autopassport/express-server/routes/index.js
+++ backend-autopassport/express-server/routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const carRoutes = require('./carRoutes');
-const brandRoutes = require('./brandRoutes');
 
 router.use('/cars', carRoutes);
-router.use('/brands', brandRoutes);
 
 module.exports = router;
--- frontend-autopassport/next.config.js
+++ frontend-autopassport/next.config.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+}
+
+nextConfig.publicRuntimeConfig = {
+  SMART_CONTRACT_ADDRESS: process.env.SMART_CONTRACT_ADDRESS,
 }
 
 module.exports = nextConfig
