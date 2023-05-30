833e24cdae5598ab94b0b59d91dc4ec0264d360e
FrancoRG32 <133518868+FrancoRG32@users.noreply.github.com>
2023-05-30 16:39:10 -0300

    Update fuelService.js

--- frontend-autopassport/services/firebase/fuelService.js
+++ frontend-autopassport/services/firebase/fuelService.js
@@ -1,8 +1,5 @@
 import firebase from '../../config/firebase'
 export async function getAllFuels() {
-  // return instance.get("sites/MLA/search?q=ipod")
-  // return fetch("https://api.mercadolibre.com/sites/MLA/search?q="+buscar)
-  // .then(res=>res.json())
   const querySnapshot = await firebase.db.collection("fuelTypes")
     .get()
   return querySnapshot.docs
