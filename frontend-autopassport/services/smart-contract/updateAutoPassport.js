bbc11e05de50f31d0a0cf1e7be9b509a24adcf71
Luciano <legarcia2@teco.com.ar>
2023-06-06 11:11:19 -0300

    Se realiza fix y refactor al actualizar tokens

--- frontend-autopassport/services/smart-contract/updateAutoPassport.js
+++ frontend-autopassport/services/smart-contract/updateAutoPassport.js
@@ -1,31 +1,37 @@
 export async function updateAutoPassport(contract, data) {
-    let { vin, mileage, repair_history, maintenance_history, newURI, last_update } = data;
+    let { vin, mileage, newRepair, newMaintenance, newURI, last_update } = data;
 
-    if (typeof repair_history === 'undefined' || repair_history.length === 0) {
-        repair_history = ""
+    // if (typeof newRepair === 'undefined') {
+    //     newRepair = ""
+    // }
+    
+    // if (newRepair && newRepair !== "") {
+    //     newRepair = JSON.stringify(newRepair)
+    //     console.log(newRepair);
+    //     newRepair = newRepair.replace(/"/g, '\\"')
+    // } 
+    
+    // if (typeof newMaintenance === 'undefined') {
+    //     newMaintenance = ""
+    // }
+    
+    // if (newMaintenance && newMaintenance !== "") {
+    //     newMaintenance = JSON.stringify(newMaintenance)
+    //     console.log(newMaintenance);
+    //     newMaintenance = newMaintenance.replace(/"/g, '\\"')
+    // } 
+
+    function process(repairOrMaintenance) {
+        return (typeof repairOrMaintenance !== 'undefined' && repairOrMaintenance !== "") ? JSON.stringify(repairOrMaintenance).replace(/"/g, '\\"') : "";
     }
-    
-    if (repair_history[0]) {
-        repair_history = JSON.stringify(repair_history[0])
-        console.log(repair_history);
-        repair_history = repair_history.replace(/"/g, '\\"')
-    } 
-    
-    if (typeof maintenance_history === 'undefined' || maintenance_history.length === 0) {
-        maintenance_history = ""
-    }
-    
-    if (maintenance_history[0]) {
-        maintenance_history = JSON.stringify(maintenance_history[0])
-        console.log(maintenance_history);
-        maintenance_history = maintenance_history.replace(/"/g, '\\"')
-    } 
+    newRepair = process(newRepair);
+    newMaintenance = process(newMaintenance);  
     
     const transaction = await contract.updateAutoPassport(
         vin,
         mileage,
-        repair_history,
-        maintenance_history,
+        newRepair,
+        newMaintenance,
         newURI,
         last_update
     );
